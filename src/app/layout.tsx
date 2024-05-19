483b0e7651d213f25d3c3add9f32a0f431b98f49
leeya018 <leeyahav018@gmail.com>
2024-05-19 17:04:45 +0300

    fotn not work

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+// import url('https://fonts.googleapis.com/css2?family=Danfo&display=swap');
 
 const inter = Inter({ subsets: ["latin"] });
 
