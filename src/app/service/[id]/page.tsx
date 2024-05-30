cee074684e6915197c23555216d6a8307ee4b77f
leeya018 <leeyahav018@gmail.com>
2024-05-30 17:34:33 +0300

    show the data fo th e  relocation-residency

--- src/app/service/[id]/page.tsx
+++ src/app/service/[id]/page.tsx
@@ -23,10 +23,42 @@ const ServicePage = () => {
   if (!chosenService) {
     throw new Error("id is not exsists");
   }
+  const renderContent = (key: string, content: any) => {
+    if (typeof content === "string") {
+      return <div>{content}</div>;
+    }
+
+    if (Array.isArray(content)) {
+      return (
+        <ul>
+          {content.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      );
+    }
+
+    if (typeof content === "object") {
+      return (
+        <div>
+          {Object.entries(content).map(([subKey, subValue]) => (
+            <div key={subKey}>
+              <h3 className="title">
+                {subKey !== "description" && subKey.replace(/_/g, " ")}
+              </h3>
+              {renderContent(subKey, subValue)}
+            </div>
+          ))}
+        </div>
+      );
+    }
+
+    return null;
+  };
 
   return (
-    <main className={`flex w-full  lg:w-[80%] lg:mx-auto flex-col `}>
-      <h1 className="title">{chosenService.title}</h1>
+    <main className={`flex w-full lg:w-[80%] lg:mx-auto flex-col `}>
+      {/* <h1 className="title">{chosenService.title}</h1>
       <ul className="flex flex-col gap-2">
         {chosenService.description.map((item: any, key: number) => (
           <li key={key} className="list-disc list-inside ">
@@ -34,10 +66,19 @@ const ServicePage = () => {
             {item}
           </li>
         ))}
-      </ul>
+      </ul> */}
+      {/* <div>{JSON.stringify(chosenService.details)}</div> */}
+      <div>
+        {Object.entries(chosenService.details).map(([key, value]) => (
+          <div key={key}>
+            <h2 className="title">{key.replace(/_/g, " ")}</h2>
+            {renderContent(key, value)}
+          </div>
+        ))}
+      </div>
 
       <Image
-        className="rounded-md  mb-4 h-[500px] w-[500px] mx-auto mt-10"
+        className="rounded-md mb-4 h-[500px] w-[500px] mx-auto mt-10"
         alt={chosenService.title}
         width={500}
         height={500}
