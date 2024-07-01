dfa9900eac4f2276f095a5bdecf778f3b295d27b
leeya018 <leeyahav018@gmail.com>
2024-07-02 01:43:51 +0300

    build

--- src/pages/api/whatsapp.ts
+++ src/pages/api/whatsapp.ts
@@ -1,23 +1,24 @@
-import { NextApiRequest, NextApiResponse } from "next";
+// import { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  // Extract data from the request body
-  const accountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
-  const authToken = "your_auth_token";
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   // Extract data from the request body
+//   const accountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+//   const authToken = "your_auth_token";
 
-  const client = require("twilio")(accountSid, authToken);
+//   const client = require("twilio")(accountSid, authToken);
 
-  client.messages
-    .create({
-      body: "Hello from twilio-node",
-      to: "+12345678901", // Text your number
-      from: "+12345678901", // From a valid Twilio number
-    })
-    .then((message: string) => console.log(message.sid))
-    .catch((err: any) => {
-      console.log(error);
-    });
-}
+//   client.messages
+//     .create({
+//       body: "Hello from twilio-node",
+//       to: "+12345678901", // Text your number
+//       from: "+12345678901", // From a valid Twilio number
+//     })
+//     // .then((message: string) => console.log(message?.sid))
+//     .then((message: string) => console.log(message))
+//     .catch((err: any) => {
+//       console.log(err);
+//     });
+// }
